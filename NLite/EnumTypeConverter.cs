86b86ddb8e5170df3cb6c5a166c2c8885d561bf8
netcasewqs <netcasewqs@gmail.com>
2013-07-30 15:33:21 +0800

    提交遗漏的文件

---
+++ NLite/EnumTypeConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace NLite.ComponentModel
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class EnumTypeConverter : TypeConverter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="destinationType"></param>
+        /// <returns></returns>
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="culture"></param>
+        /// <param name="value"></param>
+        /// <param name="destinationType"></param>
+        /// <returns></returns>
+        public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
+        {
+            if (value == null)
+                return null;
+            Enum enumValue = value as Enum;
+            if (enumValue == null)
+                return null;
+
+            return enumValue.GetDescription();
+        }
+    }
+}
